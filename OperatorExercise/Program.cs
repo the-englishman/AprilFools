a290a38c0c4084390e2d52e5c4c628a2608aa712
Tim English <timothyenglish@icloud.com>
2021-04-02 18:29:50 -0500

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b
+            int remainder = a % b
+            if (a == 17 && b == 4)
+
+            17 / 4 is 4 remainder 1
+                Use string interpoltion
+                Sta inside the Main () method's scope
+        }
+    }
+}
+
+
+
+
+Create 2 integer variables
+Use these variables:
+int a = 17;
+int b = 4;
+Create 2 more integer variables
+Use these variables:
+int quotient = a / b;
+int remainder = a % b;
+Use an if statement:
+if (a == 17 && b == 4)
+    Write out the results in the following form: 
+17 / 4 is 4 remainder 1
+Use string interpolation 
+Stay inside the Main() method’s scope
